b6cffd618a304082fe52d8f6ee00a29195177592
Sidharth45 <sidharthburji1817@gmail.com>
2026-01-29 16:13:41 +0530

    Database Fixtures

--- Accounts/api/config.php
+++ Accounts/api/config.php
@@ -3,10 +3,10 @@
 date_default_timezone_set('Asia/Kolkata');
 
 // Database configuration
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'finance_dashboard');
+define('DB_HOST', '82.25.121.32');
+define('DB_USER', 'u164024082_console');
+define('DB_PASS', 'Trishaki@tech-consoledb#304');
+define('DB_NAME', 'u164024082_console');
 
 // Create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
